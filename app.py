a972267a1f66c51d37830d1ca56d8b67e816ecf2
unknown <sbnj.tamo@gmail.com>
2025-10-25 23:30:27 +0100

    fix prob

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import joblib
 import numpy as np
 
 try:
-    model = joblib.load("student_performance_pipeline.joblib")
+    model = joblib.load("student_performance_pipeline6.joblib")
     print("✅ Model loaded successfully!")
 
     
@@ -14,34 +14,6 @@ except Exception as e:
 
 
 
-
-
-mean_values = {
-    'Hours_Studied': 19.975329,
-    'Attendance': 79.977448,
-    'Parental_Involvement': 1.086423,
-    'Access_to_Resources': 1.100197,
-    'Extracurricular_Activities': 0.596035,
-    'Sleep_Hours': 7.029060,
-    'Previous_Scores': 75.070531,
-    'Motivation_Level': 0.906463,
-    'Internet_Access': 0.924474,
-    'Tutoring_Sessions': 1.493719,
-    'Family_Income': 0.787649,
-    'Teacher_Quality': 1.195247,
-    'School_Type': 0.304071,
-    'Physical_Activity': 2.967610,
-    'Learning_Disabilities': 0.105191,
-    'Parental_Education_Level': 0.696080,
-    'Distance_from_Home': 0.501589,
-    'Gender': 0.422733,
-    'Peer_Negative': 0.208415,
-    'Peer_Neutral': 0.392311,
-    'Peer_Positive': 0.399273,
-    'Study_Attendance': 1596.890571,
-    'Sleep_Efficiency': 0.374639,
-    'Physical_Impact': 20.858937
-}
 app = Flask(__name__)
 @app.route('/')
 def home():
@@ -61,21 +33,12 @@ def predict():
             return jsonify({'error': 'Invalid input. Expected 6 features.'}), 400
         
 
-
-        input_features_names = [
-        'Hours_Studied', 'Attendance', 'Parental_Involvement',
-        'Teacher_Quality', 'Family_Income', 'Previous_Scores'
-        ]
-        user_inputs = dict(zip(input_features_names, features_list))
-
-        all_features = mean_values.copy()
-        all_features.update(user_inputs)
-
-
-        feature_order = list(mean_values.keys())
-        features = np.array([all_features[name] for name in feature_order], dtype=float)
+        features = np.array(features_list, dtype=float)
+        
         features_2d = features.reshape(1, -1)
+
         prediction = model.predict(features_2d)
+
         return jsonify({'prediction': prediction[0]})
     except Exception as e:
         print(f"Error during prediction: {e}")
